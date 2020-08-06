3a651775d6f60a2fbac1c9547085a79d7a58dfab
earth15032545 <ket15032545@gmail.com>
2020-08-06 13:40:35 +0700

    first

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char name[100],nickname[100];
+	int age, birthday;
+	printf("YOU NAME : ");
+	scanf("%s", &name);
+	printf("YOU NICKNAME : ");
+	scanf("%s", &nickname);
+	printf("YOU AGE : ");
+	scanf("%d", &age);
+	printf("YOU BIRTHDAY : ");
+	scanf("%d", &birthday);
+}
