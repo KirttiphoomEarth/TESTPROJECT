ccf964a0ca50d468c7b431c46d820286210e5e66
earth15032545 <ket15032545@gmail.com>
2020-08-06 13:58:30 +0700

    two uodate

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	char name[100],nickname[100];
-	int age, birthday;
-	printf("YOU NAME : ");
+	char name[100], nickname[100];
+	int age, birthday, weight, Height;
+	printf("You Name : ");
 	scanf("%s", &name);
-	printf("YOU NICKNAME : ");
+	printf("You Nickname : ");
 	scanf("%s", &nickname);
-	printf("YOU AGE : ");
+	printf("You Age : ");
 	scanf("%d", &age);
-	printf("YOU BIRTHDAY : ");
+	printf("You Weight : ");
+	scanf("%d", &weight);
+	printf("You Height : ");
+	scanf("%d", &Height);
+	printf("You Birthday : ");
 	scanf("%d", &birthday);
 }
